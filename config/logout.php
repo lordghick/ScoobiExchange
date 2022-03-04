5b98c084f58dffe82154a7766382813841fa1ae3
lordghick <lordghick@gmail.com>
2022-03-04 08:56:10 -0400

    Removed console log

--- config/logout.php
+++ config/logout.php
@@ -3,6 +3,5 @@
 include_once 'controlSesion.inc.php';
 include_once 'redireccion.inc.php';
 
-echo "<script>console.log('aqui tamo');</script>";
 ControlSesion :: cerrarSesion();
 Redireccion :: redirigir('../dist/index.php');
